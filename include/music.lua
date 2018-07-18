d469c08c37d8ee930aec7cfe1b1369f616d7f75f
Eneko Osia <eneko.osia@gmail.com>
2018-07-18 22:18:42 +0200

    [awesome] music command moved to a separate file

--- include/music.lua
+++ include/music.lua
@@ -23,5 +23,4 @@ end
 function spotify_stop()
     awful.util.spawn_with_shell("dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Stop")
 end
-
 -- }}}
