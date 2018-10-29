9376500efe64c31257ac359d3a5689b922360ad2
eneko-osia <eneko.osia@gmail.com>
2018-10-29 13:01:17 +0100

    [awsome] primary screen used

--- include/screens.lua
+++ include/screens.lua
@@ -1,8 +1,9 @@
 -- {{{ Screens definitions
 return
 {
-    SCREEN_CENTRE =   1 <= screen:count() and 1 or 1,
-    SCREEN_RIGHT =    2 <= screen:count() and 2 or 1,
-    SCREEN_LEFT =     3 <= screen:count() and 3 or 1
+    SCREEN_CENTRE =   1 <= screen:count() and 1 or screen.primary.index,
+    SCREEN_RIGHT =    2 <= screen:count() and 2 or screen.primary.index,
+    SCREEN_LEFT =     3 <= screen:count() and 3 or screen.primary.index,
+    SCREEN_MAX = 3
 }
 -- }}}
